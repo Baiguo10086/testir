6bb1e8de15c361556714d1f85c222e8d8ac06e7e
Baiguo10086 <77435531+Baiguo10086@users.noreply.github.com>
2024-01-14 18:36:59 +0800

    Add files via upload

--- test.s
+++ test.s
@@ -46,32 +46,28 @@ label2:
   move $v0, $t0
   jr $ra
 main:
-  lw $t1, array + 12 
   addi $sp, $sp, -4
   sw $ra, 0($sp)
   jal read
   lw $ra, 0($sp)
   addi $sp, $sp, 4
-  move $t1, $v0
-  lw $t2, array + 12 
-  move $s0, $t2
-  sw $s0, array + 16 
+  move $s0, $v0
+  lw $t1, array + 12 
+  move $s1, $t1
+  sw $s1, array + 16 
   lw $t0, array + 16 
   li $t5, 1
   bgt $t0, $t5, label3
   j label4
 label3:
-  addi $sp, $sp, -12 
+  addi $sp, $sp, -8 
   sw $ra, 0($sp)
   sw $a0, 4($sp)
-  sw $a1, 8($sp)
   lw $a0, array + 16
-  lw $a1, array + -1
   jal fact
   lw $ra, 0($sp)
   lw $a0, 4($sp)
-  lw $a1, 8($sp)
-  addi $sp, $sp, 12 
+  addi $sp, $sp, 8 
   move $s0, $v0
   sw $s0, array + 20
   lw $t0, array + 20 
